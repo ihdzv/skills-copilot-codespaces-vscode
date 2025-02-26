0fad6d0b6cff4fe168d03277b990e7c100abb846
Ismael Hernandez Villagomez <ihernandezv0501@egresado.ipn.mx>
2025-02-26 18:13:16 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,30 @@
+// Create web server
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// In-memory database
+let comments = [];
+
+// Routes
+app.get('/comments', (req, res) => {
+  res.json(comments);
+});
+
+app.post('/comments', (req, res) => {
+  const comment = req.body;
+  comments.push(comment);
+  res.status(201).json(comment);
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
