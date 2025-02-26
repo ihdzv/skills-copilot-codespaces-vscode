78746f0ac8619fa82ffe2b0f65da320d3840398e
Ismael Hernandez Villagomez <ihernandezv0501@egresado.ipn.mx>
2025-02-26 18:09:29 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+  const skills = [
+    {
+      name: "HTML",
+      level: 95,
+    },
+    {
+      name: "CSS",
+      level: 90,
+    },
+    {
+      name: "JavaScript",
+      level: 85,
+    },
+    {
+      name: "React",
+      level: 80,
+    },
+    {
+      name: "Node.js",
+      level: 75,
+    },
+  ];
+
+  return skills;
+}
